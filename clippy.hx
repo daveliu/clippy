65ec96de348c277b0917b4781044a2b94c8e6377
wenju <liuwenju26@gmail.com>
2009-02-25 18:56:23 +0800

    modify text to Chinese

--- clippy.hx
+++ clippy.hx
@@ -15,7 +15,7 @@ class Clippy {
     var label:TextField = new TextField();
     var format:TextFormat = new TextFormat("Arial", 10);
     
-    label.text = "copy to clipboard";
+    label.text = "复制到剪贴板";
     label.setTextFormat(format);
     label.textColor = 0x888888;
     label.selectable = false;
@@ -35,7 +35,7 @@ class Clippy {
     
     button.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) {
       flash.system.System.setClipboard(text);
-      label.text = "copied!";
+      label.text = "已复制!";
       label.setTextFormat(format);
     });
     
@@ -45,7 +45,7 @@ class Clippy {
     
     button.addEventListener(MouseEvent.MOUSE_OUT, function(e:MouseEvent) {
       label.visible = false;
-      label.text = "copy to clipboard";
+      label.text = "复制到剪贴板";
       label.setTextFormat(format);
     });
     
